5cee6a0ee7be16563181512135863e60e6108bf0
blazey08 <56100464+blazey08@users.noreply.github.com>
2021-04-18 13:27:35 +0800

    working version

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -15,22 +14,4 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 
 
-=======
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_mail import Mail
-import firebase_admin
-from firebase_admin import credentials, firestore, storage, db
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-mail = Mail(app)
-
-
->>>>>>> 7051cf6f23955ab084e2c4ceb770357b960ec771
 from app import routes, models
